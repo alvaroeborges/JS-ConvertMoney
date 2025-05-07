a1573330a32c24e24013c47d60142aafd146e53f
alvaroeborges <alvaro.eborges@gmail.com>
2025-05-07 19:57:32 -0300

    Math

---
+++ aula JS/math.js
@@ -0,0 +1,24 @@
+/* Math -> Matemática
+
+    - pow -> (2,2) Potência / 2² = 4
+    - sqrt (25,2) -> raiz -> √25
+    - PI 
+    - ceil ->Teto --> Arredonda para cima
+    - floor ->Chão --> arrendonda para baixo
+
+    - random () --> Número aleatório entre 0 e 1
+*/
+
+const result = Math.pow(2,2)
+
+
+console.log(result)
+
+/*
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min; 
+  }
+  */
