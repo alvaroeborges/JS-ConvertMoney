c0abf21c0d758061525a127513260db3cf59f5eb
alvaroeborges <alvaro.eborges@gmail.com>
2025-05-13 21:28:33 -0300

    comparação

---
+++ aula JS/comparação.js
@@ -0,0 +1,36 @@
+/*
+    Operadores de Comparação
+
+    == -> Igual // Compara o valor, mas não o tipo
+
+    === -> Totalmente igual // Compara o VALOR e o TIPO
+
+    != -> Diferente // Ele compara o valor, mas não o tipo
+
+    !== -> TOTALMENT Diferente // Compara o VALOR e o TIPO.
+
+    >  -> Maior
+    <  -> Menos
+
+    >= -> Maior igual
+
+    <= -> Menor igual
+
+*/
+
+
+const  firstNumber = 30
+const seccondNumber = "30"
+
+console.log (firstNumber !== seccondNumber)
+
+/*
+if(firstNumber == seccondNumber){
+    console.log("São iguais")
+
+}else {
+    console.log("Não são iguais")
+
+}
+*/
+
