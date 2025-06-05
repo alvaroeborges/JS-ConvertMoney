d3b32d1651b3cd7defb482d6421520905b989d98
alvaroeborges <alvaro.eborges@gmail.com>
2025-06-05 20:35:30 -0300

    filter

--- aula JS/filter.js
+++ aula JS/filter.js
@@ -29,28 +29,37 @@ console.log(newList)
 
 
 
-     const compaines = [
+const compaines = [
 
-        {name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938},
-        {name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975},
-        {name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968},
-        {name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004},
-        {name: 'Spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006},
-        {name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976},
- ];
+    { name: 'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938 },
+    { name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975 },
+    { name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968 },
+    { name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004 },
+    { name: 'Spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006 },
+    { name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976 },
+];
 
-const newCompanies = compaines.filter((comp) => {
-    if (comp.marketValue < 200  && comp.foundedOn > 1990) {
+const newCompaines = compaines.map(compaines => {
+    return {
+        newValue: compaines.marketValue - (compaines.marketValue * 0.1)
+        
+    }
+
+})
+
+const newList = compaines.filter((compaines) => {
+    if (compaines.foundedOn > 1990) {
         return true;
     } else {
         return false;
-    }
-});
+    }   
+    
+ })
+
+const newTotal = newCompaines.reduce((acumulador, valorAtual) => {
+    return acumulador + valorAtual.newValue;
+}, 0)
 
 
-console.log(newCompanies)
 
-
-
-
-
+console.log(newCompaines, newList, newTotal)
